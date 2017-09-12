1edc909e2d3824747ca5aa1c347aa21a7a00976d
Kunal Hire <kunalh@inhirekunal.local>
2017-09-12 13:45:58 +0530

    Added leave form

---
+++ src/containers/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LeaveForm from '../components/leaves/LeaveForm'
+
+const App = () => (
+    <div>
+        <h2>Leave Management System</h2>
+        <LeaveForm />
+    </div>
+);
+
+export default App
